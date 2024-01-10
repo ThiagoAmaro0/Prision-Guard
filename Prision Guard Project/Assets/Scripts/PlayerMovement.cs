95671d57d7ced24d65f458b47ba4d91f5f892857
Thiago Amaro <thiagoamaro010399@gmail.com>
2024-01-10 20:45:43 -0300

    Move System

---
+++ Prision Guard Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrisionGuard
+{
+
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField] private float _moveSpeed = 10;
+        [SerializeField] private Animator _animator;
+        [SerializeField] private Joystick _joystick;
+        [SerializeField] private Rigidbody _rb;
+
+        void FixedUpdate()
+        {
+            if (_rb)
+            {
+                if (_joystick)
+                {
+                    if (_joystick.Direction != Vector2.zero)
+                    {
+                        _rb.velocity = new Vector3(_joystick.Direction.x, 0, _joystick.Direction.y) * _moveSpeed * Time.deltaTime;
+                        _animator.transform.forward = _rb.velocity.normalized;
+                    }
+                    _animator.SetBool("Walking", _joystick.Direction != Vector2.zero);
+                }
+                else
+                {
+                    Debug.LogError("Player's joystick not serialized!", this);
+                }
+            }
+            else
+            {
+                Debug.LogError("Player's Rigidbody not serialized!", this);
+            }
+        }
+    }
+}
